e920dcd8d45a52f6c6a46da82f35a93568c412f8
BHUVANA RAJU G <105202669+Bhuvana-Raju-G@users.noreply.github.com>
2023-06-05 15:24:33 +0530

    Add files via upload

---
+++ demo_Authentication_demo/src/main/java/com/example/demo/authentication/demo/DemoAuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.authentication.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthenticationDemoApplication.class, args);
+	}
+
+}
